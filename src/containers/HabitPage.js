1ff096d84b0ab857ffd46f5c0201bdd7f25c5b08
Andrew Park <andrewpark@Andrews-MacBook-Pro.local>
2018-10-04 15:34:00 -0400

    renders HabitPage as initial page

---
+++ src/containers/HabitPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HabitPage extends Component {
+  render() {
+    return (
+      <div>
+        Habit Page 
+        {/* <HabitTable /> */}
+        {/* <HabitList /> */}
+      </div>
+    );
+  }
+}
+
+export default HabitPage;
