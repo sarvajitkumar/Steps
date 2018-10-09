7c75583ad365a837829d07ac4d111b7754328122
Andrew Park <andrewpark@Andrews-MacBook-Pro.local>
2018-10-08 20:40:52 -0400

    create api file to connect to db and fetch habits

---
+++ src/utils/api.js
@@ -0,0 +1,26 @@
+import DataStore from 'nedb';
+
+function loadDatabase() {
+  return new DataStore({
+    filename: 'steps/habitsData',
+    timestampData: true,
+    autoload: true
+  });
+}
+
+//fetch habits
+export function fetchInitialHabits() {
+  return new Promise((res, rej) => {
+    const db = loadDatabase();
+
+    db.find({}).sort({createdAt: 1})
+      .exec((err, habits) => {
+        if (err) rej(err);
+        res(habits);
+      })
+  })
+}
+
+//add habit
+//update habit
+//remove habit
