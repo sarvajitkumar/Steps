1c4782e062ac73027df60e32709dc32427a8ed8f
Andrew Park <andrewpark@Andrews-MacBook-Pro.local>
2018-10-16 16:47:19 -0400

    add react router in order to open new settings window directly to HabitListItemSettings component this is in order to overlap the window over the mainWindow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import './styles/Resizer.css';
 import './components/HabitTable/style.css';
 import './components/HabitList/style.scss';
 import App from './containers/App';
 import * as serviceWorker from './utils/serviceWorker';
+import HabitListItemSettings from './components/HabitList/HabitListItemSettings';
 
 render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={App} />
+        <Route path="/habit-settings" component={HabitListItemSettings} />
+      </Fragment>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
