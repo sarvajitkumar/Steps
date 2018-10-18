5215fc8f306c837e400756d7a45faa4928747221
Andrew Park <andrewpark@Andrews-MacBook-Pro.local>
2018-10-18 17:59:29 -0400

    use emotion for habitBox

--- src/components/HabitTable/HabitBox.js
+++ src/components/HabitTable/HabitBox.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
+
+const habitBoxStyles = css`
+  width: 30px;
+  height: 30px;
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+  cursor: pointer;
+
+  &:hover {
+    background-color: grey;
+  }
+`
 
 const HabitBox = (props) => {
   return (
-    <div className="habit-box"
+    <div className={habitBoxStyles}
           style={{backgroundColor: props.backgroundColor}}
           onClick={props.onClick} />
   );
